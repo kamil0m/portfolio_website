d386a8968bfea5013b2da92b1d25716b7eabf5f5
Kamil Markowski <kamil0markowski@gmail.com>
2024-10-15 12:22:01 +0200

    coding environment configured with vite typescript and react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+function App() {
+
+ return (
+  <div className="App">
+   <div className="navbar">
+    <ul>
+     <li>Home</li>
+     <li>About me</li>
+     <li>My projects</li>
+     <li>My skills</li>     
+     <li>Contact</li>
+    </ul>
+   </div>
+  </div>
+ )
+}
+export default App
