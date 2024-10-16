abba8a8994de74799dfc8b6a18f8db38d6ae7fb0
Kamil Markowski <kamil0markowski@gmail.com>
2024-10-16 19:17:54 +0200

    about me photo imported

--- src/components/pages/Aboutme.tsx
+++ src/components/pages/Aboutme.tsx
@@ -1,5 +1,9 @@
+
+import aboutmePhoto from '../../assets/KM_aboutme2.jpeg';
+
 export function Aboutme() {
     return <>
         <h1>Kamil MARKOWSKI</h1>
+        <img src={aboutmePhoto} className="about__photo"></img>
     </>
 }
