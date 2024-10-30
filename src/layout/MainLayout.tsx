f5886c4344b175ad46db090c479057c5f16297ed
Kamil Markowski <kamil0markowski@gmail.com>
2024-10-30 10:40:05 +0100

    settings styles added

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,15 +6,17 @@ import {
 export function MainLayout() {
     return (
         <>
-            <nav className='navbar' >
-                <ul className="navbar__links">
-                    <NavLink className="navbar__element" to="/">Home</NavLink>
-                    <NavLink className="navbar__element" to="/aboutme">About me</NavLink>
-                    <NavLink className="navbar__element" to="/projects">My projects</NavLink>
-                    <NavLink className="navbar__element" to="/skills">My skills</NavLink>     
-                    <NavLink className="navbar__element" to="/contact">Contact</NavLink>
-                </ul>
-            </nav>
+            <div className='container'>    
+                <nav className='navbar' >
+                    <ul className="navbar__links">
+                        <NavLink className="navbar__element" to="/">Home</NavLink>
+                        <NavLink className="navbar__element" to="/aboutme">About me</NavLink>
+                        <NavLink className="navbar__element" to="/projects">My projects</NavLink>
+                        <NavLink className="navbar__element" to="/skills">My skills</NavLink>     
+                        <NavLink className="navbar__element" to="/contact">Contact</NavLink>
+                    </ul>
+                </nav>
+            </div>
             <Outlet/>
         </>
     )
