70b9d44e1edf7e5826bedf6d62f66785a988587f
Kamil Markowski <kamil0markowski@gmail.com>
2024-11-03 20:51:31 +0100

    navbar styling improved

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,27 +3,50 @@ import {
     Outlet
 } from 'react-router-dom';
 
+// export function MainLayout() {
+//     return (
+//         <>
+//             <div className='background'></div>
+//             <div className='container navbar__container'>    
+//                 <nav className='navbar' >
+//                     <ul className="navbar__links">
+//                         <li><NavLink className="navbar__element" to="/" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>Home</NavLink></li>
+//                         <li>
+//                             <NavLink className="navbar__element" to="/aboutme">About me</NavLink>
+//                         </li>
+//                         <li className='tab-current'>
+//                             <NavLink className="navbar__element" to="/projects">My projects</NavLink>
+//                         </li>
+//                         <li>
+//                             <NavLink className="navbar__element" to="/skills">My skills</NavLink>     
+//                         </li>
+//                         <li>
+//                             <NavLink className="navbar__element" to="/contact">Contact</NavLink>
+//                         </li>
+//                     </ul>
+//                 </nav>
+//             </div>
+//             <Outlet/>
+//         </>
+//     )
+// }
+
 export function MainLayout() {
     return (
         <>
             <div className='background'></div>
             <div className='container navbar__container'>    
                 <nav className='navbar' >
-                    <ul className="navbar__links">
-                        <li><NavLink className="navbar__element" to="/">Home</NavLink></li>
-                        <li>
-                            <NavLink className="navbar__element" to="/aboutme">About me</NavLink>
-                        </li>
-                        <li className='tab-current'>
-                            <NavLink className="navbar__element" to="/projects">My projects</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="navbar__element" to="/skills">My skills</NavLink>     
-                        </li>
-                        <li>
-                            <NavLink className="navbar__element" to="/contact">Contact</NavLink>
-                        </li>
-                    </ul>
+                    <NavLink className="navbar__element" to="/" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>Home</NavLink>
+
+                    <NavLink className="navbar__element" to="/aboutme" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>About me</NavLink>
+
+                    <NavLink className="navbar__element" to="/projects" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>My projects</NavLink>
+                
+                    <NavLink className="navbar__element" to="/skills" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>My skills</NavLink>     
+                
+                    <NavLink className="navbar__element" to="/contact" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>Contact</NavLink>
+                    
                 </nav>
             </div>
             <Outlet/>
