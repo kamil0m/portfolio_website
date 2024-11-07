7a44321b80054c1b253c6c8e985af0c902208702
Kamil Markowski <kamil0markowski@gmail.com>
2024-11-07 23:04:16 +0100

    background issue fixed

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,33 +3,6 @@ import {
     Outlet
 } from 'react-router-dom';
 
-// export function MainLayout() {
-//     return (
-//         <>
-//             <div className='background'></div>
-//             <div className='container navbar__container'>    
-//                 <nav className='navbar' >
-//                     <ul className="navbar__links">
-//                         <li><NavLink className="navbar__element" to="/" className={({ isActive }) => isActive ? 'navbar__element tab-current' : 'navbar__element'}>Home</NavLink></li>
-//                         <li>
-//                             <NavLink className="navbar__element" to="/aboutme">About me</NavLink>
-//                         </li>
-//                         <li className='tab-current'>
-//                             <NavLink className="navbar__element" to="/projects">My projects</NavLink>
-//                         </li>
-//                         <li>
-//                             <NavLink className="navbar__element" to="/skills">My skills</NavLink>     
-//                         </li>
-//                         <li>
-//                             <NavLink className="navbar__element" to="/contact">Contact</NavLink>
-//                         </li>
-//                     </ul>
-//                 </nav>
-//             </div>
-//             <Outlet/>
-//         </>
-//     )
-// }
 
 export function MainLayout() {
     return (
