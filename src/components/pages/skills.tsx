4db88fc48c6205ba02a08f1685d130fbf6e0166e
Kamil Markowski <kamil0markowski@gmail.com>
2024-11-16 14:45:36 +0100

    other skills integrated to the skills from projects

--- src/components/pages/skills.tsx
+++ src/components/pages/skills.tsx
@@ -1,15 +1,16 @@
 import projects from "../../assets/projects.json";
+import skills from "../../assets/skills.json";
 
 export const Skills: React.FC = () => {
 
-    // écrire une liste des compétences qui pourrait ensuite être concatené avec celle éxtraite des projets
-
-    const uniqueSkills = Array.from(new Set(projects.flatMap(project =>
+    const projectsSkills = projects.flatMap(project =>
         project.technologiesUsed.map(tech => tech.name)
-    )));
+    );
+
+    const uniqueSkills = Array.from(new Set([...projectsSkills, ...skills]));
 
     return <div className="container skills__container">
-        <h1 className="skills__title">Here are some of my skills:</h1>
+        <h1 className="skills__title">Here's what I can do:</h1>
         <ul className="skills__list">
             
             {uniqueSkills.map((skill, index) => (
