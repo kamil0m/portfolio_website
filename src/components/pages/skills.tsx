70c40a62cb9a9984e1f4f1b802fded675d5c701e
Kamil Markowski <kamil0markowski@gmail.com>
2024-11-16 11:37:37 +0100

    basic skills stylng added

--- src/components/pages/skills.tsx
+++ src/components/pages/skills.tsx
@@ -2,16 +2,20 @@ import projects from "../../assets/projects.json";
 
 export const Skills: React.FC = () => {
 
-    // écrire une liste des compétences
+    // écrire une liste des compétences qui pourrait ensuite être concatené avec celle éxtraite des projets
 
     const uniqueSkills = Array.from(new Set(projects.flatMap(project =>
         project.technologiesUsed.map(tech => tech.name)
     )));
 
-    return <ul className="skill-entry">
-        {uniqueSkills.map((skill, index) => (
-            <li key={index}>{skill}</li>
-        )
-        )}
-    </ul>
+    return <div className="container skills__container">
+        <h1 className="skills__title">Here are some of my skills:</h1>
+        <ul className="skills__list">
+            
+            {uniqueSkills.map((skill, index) => (
+                <li key={index} className="skill__entry">{skill}</li>
+            ))}
+
+        </ul>
+    </div>
 };
