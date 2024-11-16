d90bc2a393c74ed9f50215fd49f92344bc27000f
Kamil Markowski <kamil0markowski@gmail.com>
2024-11-16 15:27:49 +0100

    simple contact page styling added

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -1,10 +1,13 @@
 export const Contact: React.FC = () => {
-    return <>
+    return <div className="container">
         <div className="contact__container">
-            <h2 class="contact__title">Let's get in touch! Email me on:</h2>
+            <h2 className="contact__title">
+                <span>Interested in my work?</span>
+                <span>Let's get in touch! Email me on:</span>
+            </h2>
             <a href="mailto:kamil0markowski@gmail.com" className="contact__email">
             kamil0markowski@gmail.com
             </a>
         </div>
-    </>
+    </div>
 }
