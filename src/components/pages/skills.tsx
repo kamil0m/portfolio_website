b1e944a67603d5f5ab3632ceecb9de05b9ee6513
Kamil Markowski <kamil0markowski@gmail.com>
2024-12-10 21:49:59 +0100

    skills page updated

--- src/components/pages/skills.tsx
+++ src/components/pages/skills.tsx
@@ -1,22 +1,29 @@
-import projects from "../../assets/projects.json";
+// import projects from "../../assets/projects.json";
 import skills from "../../assets/skills.json";
 
 export const Skills: React.FC = () => {
 
-    const projectsSkills = projects.flatMap(project =>
-        project.technologiesUsed.map(tech => tech.name)
-    );
-
-    const uniqueSkills = Array.from(new Set([...projectsSkills, ...skills]));
+    const uniqueCategories = Array.from(new Set(skills.map(skill => skill.category)));
+    console.log(uniqueCategories);
 
     return <div className="container skills__container">
         <h1 className="skills__title">Here's what I can do:</h1>
-        <ul className="skills__list">
-            
-            {uniqueSkills.map((skill, index) => (
-                <li key={index} className="skill__entry">{skill}</li>
-            ))}
-
-        </ul>
+        {uniqueCategories.map((category, index) => {
+            return (
+                <>
+                <h2 key={index}>{category}</h2>
+                {skills.filter(skill => skill.category === category).map((skill, index) => {
+                    return (
+                        <li key={index} className="skills__card">
+                            {/* <img src={skill.image} alt={skill.name} className="skills__image"/> */}
+                            <div className="skills__name">{skill.name}
+                                <span className="skills__description techno__tooltip">{skill.description}</span>
+                            </div>
+                        </li>
+                    )
+                })}
+                </>
+            )
+        })}
     </div>
 };
