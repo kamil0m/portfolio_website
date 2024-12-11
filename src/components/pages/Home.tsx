88290e3b772e05903dbf7801700ca1b50ff7159d
Kamil Markowski <kamil0markowski@gmail.com>
2024-12-11 15:41:43 +0100

    home page sttyling added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,6 @@
-
-
 export function Home() {
-    return <div className="container">
-        <h1 className="page__title" >Welcome to my portfolio!</h1>
-        <p>I’m Kamil Markowski, a web developer with a focus on building intuitive, dynamic and engaging front-end solutions. With a solid foundation in HTML, CSS, JavaScript, NodeJS and React, I create responsive and high-performing web applications. My goal is to turn concepts into seamless digital experiences by writing clean and efficient code. Explore my work to see how I can blend problem-solving creativity with technology to bring your ideas to life.</p>
+    return <div className="container home__container">
+        <h1 className="home__title" >Welcome to my portfolio!</h1>
+        <p className="home__text">I'm Kamil Markowski, a web developer with a focus on building intuitive, dynamic and engaging front-end solutions. With a solid foundation in HTML, CSS, JavaScript, NodeJS and React, I create responsive and high-performing web applications. My goal is to turn concepts into seamless digital experiences by writing clean and efficient code. Explore my work to see how I can blend problem-solving creativity with technology to bring your ideas to life.</p>
     </div>
 }
