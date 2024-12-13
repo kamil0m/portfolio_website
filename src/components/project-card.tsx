8245017884d506c0d97affef3ceb584815933537
Kamil Markowski <kamil0markowski@gmail.com>
2024-12-13 19:34:12 +0100

    github button added to project cards

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -11,7 +11,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     const projectImagePath = `/src/assets/projects/${project.projectId}.png`;
 
     return (
-        <a className='card__link' href={project.urlDemo}> 
+        <a className='card__link' href={project.urlDemo} target='_blank'> 
             <img src={projectImagePath} className='card__image'></img>
             <h2 className='card__title'>{project.projectName}</h2>
             <p className='project__description'>{project.description}</p>
@@ -26,8 +26,8 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                 </li>
                 ))}
             </ul>
-            <button className='card__button material-symbols-outlined'>arrow_forward</button>
-            <a className='card__button material-symbols-outlined' href={project.urlGit}>arrow_forward</a>
+            <button className='material-symbols-outlined card__button button__go'>arrow_forward</button>
+            <a className='card__button button__github' target='_blank' href={project.urlGit}><i className='fa-brands fa-github gtihub__icon'></i></a>
             
         </a>
     );
