71f67d5f0a160184985819313fc170b69dea2567
Kamil Markowski <kamil0markowski@gmail.com>
2024-12-18 01:05:20 +0100

    Versioning untracked files

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';
 import { Router } from './Router.tsx';
-// import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
     <Router />
   </StrictMode>,
 )
