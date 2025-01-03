051eccbbd1d7df5e02851bb76895df170270dcd9
Kamil Markowski <kamil0markowski@gmail.com>
2025-01-03 23:15:47 +0100

    text edited

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -3,7 +3,7 @@ export const Contact: React.FC = () => {
         <h1 className="home__title" >Contact me!</h1>
         <h2 className="contact__title">
             <span>Interested in my work?</span>
-            <span>Let's get in touch! Email me on:</span>
+            <span>Let's get in touch! Email me at:</span>
         </h2>
         <a href="mailto:kamil0markowski@gmail.com" className="contact__email">kamil0markowski@gmail.com</a>
     </div>
